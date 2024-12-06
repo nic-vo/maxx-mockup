1ccd8cf5ca7b09603c7ba9865fcb7d2de90a14bb
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-12-05 22:07:03 -0500

    feat: add custom colors and font families from maxx

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				'maxxgray':'#3d3d3d',
+				'maxxred': '#bb1f25',
+				'maxxblue': '#0d4b6e'
+			},
+			fontFamily: {
+				'helv': ['Helvetica Neue', 'sans-serif', 'ui-sans-serif', 'system-ui'],
+				'bask': ['Baskervville', 'serif','ui-serif', 'system-ui']
+			}
+		},
   },
   plugins: [],
 }
